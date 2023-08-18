c1c8d36b2bbac02cab4ca4e34d183ee20c66356b
dhubadm <142442445+dhubadm@users.noreply.github.com>
2023-08-18 19:21:29 +0530

    environment parameters

---
+++ envVars.groovy
@@ -0,0 +1,32 @@
+env.BUILD=${params.BUILD}
+env.PATCH=${params.PATCH}
+env.RELEASE_VESRION=${params.RELEASE_VERSION}
+//BUILD PART numbers parameters
+env.L1_F132=${params.L1_F132}
+env.L1_F133=${params.L1_F133}
+env.L1_F194=${params.L1_F194}
+env.L1_F195=${params.L1_F195}
+env.L2_F132=${params.L2_F132}
+env.L2_F133=${params.L2_F133}
+env.L2_F194=${params.L2_F194}
+env.L2_F195=${params.L2_F195}
+env.L1P_F132=${params.L1P_F132}
+env.L1P_F133=${params.L1P_F133}
+env.L1P_F194=${params.L1P_F194}
+env.L1P_F195=${params.L1P_F195}
+//BST BUILD tool parameters
+env.TRACKER_360=${params.TRACKER_360}
+env.SATA=${params.SATA}
+env.SPP_OTP=${params.SPP_OTP}
+env.VSE=${params.VSE}
+//JIRA FD22 Module parameters
+env.ADAPTIVE_CRUISE_CONTROL=${params.ADAPTIVE_CRUISE_CONTROL}
+env.COLLISION_AVOIDANCE=${params.COLLISION_AVOIDANCE}
+env.INHIBIT_MANAGER=${params.INHIBIT_MANAGER}
+env.HMI=${params.HMI}
+env.LATERAL_CONTROL=${params.LATERAL_CONTROL}
+env.SIDE_FEATURES=${params.SIDE_FEATURES}
+env.INPUT_WRAPPER=${params.INPUT_WRAPPER}
+env.OUTPUT_WRAPPER=${params.OUTPUT_WRAPPER}
+env.VEHICLE_STATUS=${params.VEHICLE_STATUS}
+env.SAF=${params.SAF}
